07db87fddfdb204a885816ee286f7fd47c25c7be
Forcng <164341281+Forcng@users.noreply.github.com>
2025-04-09 04:04:16 -0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Forcng'
+description 'Loading Screen Wrote in Tailwind CSS & JS'
+version '1.0'
+
+loadscreen 'index.html'
+loadscreen_cursor 'yes'
+
+client_scrips {
+    'client.lua',
+}
+
+server_scripts {
+    'server.lua',
+}
+
+files {
+    'index.html',
+}
+
+lua54 'yes'
